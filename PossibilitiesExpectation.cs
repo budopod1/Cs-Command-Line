36877aa3d46ec2d7bc5cb07fba6d1c721003c219
budopod1 <51675874+budopod1@users.noreply.github.com>
2024-05-18 22:21:11 +0000

    Added: PossibilitiesExpectation can now take values from enum

--- PossibilitiesExpectation.cs
+++ PossibilitiesExpectation.cs
@@ -11,6 +11,15 @@ public class PossibilitiesExpectation : Expectation {
         this.options = options.ToList();
     }
 
+    public PossibilitiesExpectation(int default_, Type type) {
+        options = Enum.GetNames(type).ToList();
+        int i = 0;
+        foreach (int val in Enum.GetValues(type)) {
+            if (val == default_) this.default_ = options[i];
+            i++;
+        }
+    }
+
     public override bool Matches(string word) {return options.Contains(word);}
     public override bool IsOptional() {return false;}
     public override bool IsEmpty() {return false;}
@@ -25,5 +34,10 @@ public class PossibilitiesExpectation : Expectation {
     public string Value() {
         if (Matched == null || Matched == "") return default_;
         return Matched;
+    }
+
+    public T ToEnum<T>() where T : struct {
+        Enum.TryParse(Value(), out T result);
+        return result;
     }
 }
